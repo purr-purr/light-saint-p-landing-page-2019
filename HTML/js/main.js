469b5a7b7f72cc9b0f6c1de8b2e0738237d25b7b
666hesoyam <56505654+666hesoyam@users.noreply.github.com>
2019-12-27 14:40:13 +0200

    small fix

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
         autoplay: true,
         autoplaySpeed: 2000,
         slidesToShow: 2,
-        prevArrow: "<img src='/HTML/img/icons/left-arrow.svg' class='prev' alt='prev'>",
-        nextArrow: "<img src='/HTML/img/icons/right-arrow.svg' class='next' alt='next'>",
+        prevArrow: "<img src='../HTML/img/icons/left-arrow.svg' class='prev' alt='prev'>",
+        nextArrow: "<img src='../HTML/img/icons/right-arrow.svg' class='next' alt='next'>",
         slidesToScroll: 1
     });
 });
@@ -14,8 +14,8 @@ $(document).ready(function(){
         autoplay: true,
         autoplaySpeed: 2000,
         slidesToShow: 1,
-        prevArrow: "<img src='/HTML/img/icons/left-arrow.svg' class='prev' alt='prev'>",
-        nextArrow: "<img src='/HTML/img/icons/right-arrow.svg' class='next' alt='next'>",
+        prevArrow: "<img src='../HTML/img/icons/left-arrow.svg' class='prev' alt='prev'>",
+        nextArrow: "<img src='../HTML/img/icons/right-arrow.svg' class='next' alt='next'>",
         slidesToScroll: 1
     });
 })
